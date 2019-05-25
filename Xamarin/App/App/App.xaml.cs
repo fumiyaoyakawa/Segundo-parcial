126651204b906270f8e598ad3be642db2c83458c
fumiya <fumiya.om@gmail.com>
2019-05-25 11:57:55 -0400

    creacion del view nota (pagina de contenido)

--- Xamarin/App/App/App.xaml.cs
+++ Xamarin/App/App/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace App
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Nota();
         }
 
         protected override void OnStart()
