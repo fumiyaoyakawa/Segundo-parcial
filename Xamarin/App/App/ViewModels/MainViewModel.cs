a0ba9b80b7a60e85149acdef7b54bf6958a9c295
fumiya <fumiya.om@gmail.com>
2019-05-25 12:40:23 -0400

    ViewModels e Instance locator

---
+++ Xamarin/App/App/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.ViewModels
+{
+    using Models;
+    using System;
+    using System.Collections.ObjectModel;
+    public class MainViewModel
+    {
+        #region Properties
+        public string Token { get; set; }
+        public string TokenType { get; set; }
+        #endregion
+        #region ViewModels
+        public NotaViewModel Nota { get; set; }
+        #endregion
+        #region Constructors
+        public MainViewModel()
+        {
+            instance = this;
+            this.Nota = new NotaViewModel();
+
+        }
+        #endregion
+        #region Singleton
+        private static MainViewModel instance;
+
+        public static MainViewModel GetInstcance()
+        {
+            if(instance == null)
+            {
+                return new MainViewModel();
+            }
+            return instance;
+        }
+        #endregion
+    }
+}
---
+++ Xamarin/App/App/ViewModels/NotaViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace App.ViewModels
+{
+    class NotaViewModel : BaseViewModel
+    {
+        #region variables
+        string nota;
+        bool isrunning;
+        bool isenabled;
+        #endregion
+        #region propiedades
+        public string Nota
+        {
+            get { return this.nota; }
+            set { SetValue(ref this.nota, value); }
+        }
+        public bool IsRunning
+        {
+            get { return this.isrunning; }
+            set { SetValue(ref this.isrunning, value); }
+        }
+        public bool IsEnabled
+        {
+            get { return this.isenabled; }
+            set { SetValue(ref this.isenabled, value); }
+        }
+        #endregion
+        #region command
+        public ICommand Notacmd
+        {
+            get
+            {
+                return new RelayCommand(Notacmd);
+            }
+        }
+        private async void Notacmd()
+        {
+            if (String.IsNullOrEmpty(nota))
+            {
+                await App.Current.MainPage.DisplayAlert("Nota empty",
+                    "ingrese nota",
+                    "accept");
+                return;
+            }
+            IsRunning = true;
+
+        }
+        #endregion
+    }
+}
